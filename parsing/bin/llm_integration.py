2e8dc184dd92574c62cd764395fe22df40b0b9e1
barbarastrasser <162976974+barbarastrasser@users.noreply.github.com>
2024-06-17 08:01:04 +0200

    [ENH] create json annotations for diagnosis (#21)
    * removed uneccessary code snippets
    
    * refactor + extended sex annotation
    
    * diagnosis handling
    
    * added tests
    
    * added list of snomed codes

--- parsing/bin/llm_integration.py
+++ parsing/bin/llm_integration.py
@@ -1,10 +1,10 @@
-from typing import Dict, List, Optional, Union, Any
+from typing import Dict, Union, Optional, List, Any, Callable, Mapping
 import pandas as pd
 import json
 from pydantic import BaseModel, Field
 
 
-class IsAboutBase(BaseModel):  # type: ignore
+class IsAboutBase(BaseModel):  # type:ignore
     Label: str
     TermURL: str
 
@@ -20,58 +20,36 @@ class IsAboutSession(IsAboutBase):
 
 
 class IsAboutSex(IsAboutBase):
-    Label: str = Field(default="Sex")
+    Label: str = Field(default="Sex variable")
     TermURL: str
-    Levels: Optional[Dict[str, str]] = Field(
-        default=None, description="Levels for the sex annotation"
-    )
 
 
 class IsAboutAge(IsAboutBase):
-    Label: str = Field(default="Age")
+    Label: str = Field(default="Age variable")
     TermURL: str
 
 
-class Annotations(BaseModel):  # type: ignore
-    IsAbout: Union[IsAboutParticipant, IsAboutSex, IsAboutAge, IsAboutSession]
+class IsAboutGroup(IsAboutBase):
+    Label: str = Field(default="Diagnosis variable")
+    TermURL: str
+
+
+class Annotations(BaseModel):  # type:ignore
+    IsAbout: Union[
+        IsAboutParticipant,
+        IsAboutSex,
+        IsAboutAge,
+        IsAboutSession,
+        IsAboutGroup,
+    ]
     Identifies: Optional[str] = None
     Levels: Optional[Dict[str, Dict[str, str]]] = None
     Transformation: Optional[Dict[str, str]] = None
 
-    class ConfigDict:
-        # Allow extra fields
-        extra = "allow"
-        # Exclude fields with value None
-        exclude_none = True
-
-    def dict(
-        self,
-        *,
-        exclude_unset: bool = True,  # Exclude fields that have not been set
-        **kwargs: Union[str, int, float, bool, None],
-    ) -> Dict[str, Any]:
-        model_dict: Dict[str, Any] = super().model_dump(
-            exclude_unset=exclude_unset,
-            **kwargs,
-        )
-
-        # Conditionally remove Levels if it's None
-        if model_dict.get("Levels") is None:
-            model_dict.pop("Levels", None)
-
-        # Conditionally remove Identifies if it's None
-        if model_dict.get("Identifies") is None:
-            model_dict.pop("Identifies", None)
-
-        # Conditionally remove Transformation if it's None
-        if model_dict.get("Transformation") is None:
-            model_dict.pop("Transformation", None)
-
-        return model_dict
-
 
 class TSVAnnotations(BaseModel):  # type:ignore
     Description: str
+    Levels: Optional[Dict[str, str]] = None
     Annotations: Annotations
 
 
@@ -87,27 +65,23 @@ def convert_tsv_to_dict(tsv_file: str) -> Dict[str, str]:
 def tsv_to_json(tsv_file: str, json_file: str) -> None:
     df = pd.read_csv(tsv_file, delimiter="\t")
     columns: List[str] = df.columns.tolist()
-    data = {column: "" for column in columns}
+    data: Dict[str, str] = {column: "" for column in columns}
     with open(json_file, "w") as file:
         json.dump(data, file, indent=4)
 
 
-def process_parsed_output(
-    parsed_output: Dict[str, Union[str, Any]]
-) -> Union[str, TSVAnnotations]:
-    termurl_to_model: Dict[str, type[IsAboutBase]] = {
-        "nb:ParticipantID": IsAboutParticipant,
-        "nb:Sex": IsAboutSex,
-        "nb:Age": IsAboutAge,
-        "nb:Session": IsAboutSession,
-    }
+def handle_participant(parsed_output: Dict[str, Any]) -> TSVAnnotations:
+    annotation_instance = IsAboutParticipant(TermURL=parsed_output["TermURL"])
+    description = "A participant ID"
+    annotations = Annotations(
+        IsAbout=annotation_instance, Identifies="participant"
+    )
+    return TSVAnnotations(Description=description, Annotations=annotations)
 
-    levels_mapping = {
-        "male": {"TermURL": "snomed:248153007", "Label": "Male"},
-        "female": {"TermURL": "snomed:248152002", "Label": "Female"},
-        # Add mappings for other levels as needed
-    }
 
+def handle_age(parsed_output: Dict[str, Any]) -> TSVAnnotations:
+    annotation_instance = IsAboutAge(TermURL=parsed_output["TermURL"])
+    description = "Age information"
     transformation_mapping = {
         "floatvalue": {"TermURL": "nb:FromFloat", "Label": "float value"},
         "integervalue": {"TermURL": "nb:FromInt", "Label": "integer value"},
@@ -123,87 +97,118 @@ def process_parsed_output(
             "TermURL": "nb:FromISO8601",
             "Label": "period of time according to the ISO8601 standard",
         },
-        # Add mappings for other levels as needed
+    }
+    transformation_key = parsed_output.get("Format", "").strip().lower()
+    transformation = transformation_mapping.get(transformation_key)
+    annotations = Annotations(
+        IsAbout=annotation_instance, Transformation=transformation
+    )
+    return TSVAnnotations(Description=description, Annotations=annotations)
+
+
+def handle_categorical(
+    parsed_output: Dict[str, Any], levels_mapping: Mapping[str, Dict[str, str]]
+) -> TSVAnnotations:
+    termurl = parsed_output.get("TermURL")
+
+    if termurl == "nb:Sex":
+        description = "Sex variable"
+        annotation_instance = IsAboutSex(Label="Sex", TermURL=termurl)
+    elif termurl == "nb:Diagnosis":
+        description = "Group variable"
+        annotation_instance = IsAboutGroup(Label="Diagnosis", TermURL=termurl)
+    else:
+        raise ValueError(f"Unhandled TermURL: {termurl}")
+
+    levels = {
+        key: levels_mapping.get(value.strip().lower(), {})
+        for key, value in parsed_output.get("Levels", {}).items()
     }
 
-    term_url = parsed_output.get("TermURL")
-    if isinstance(term_url, str) and term_url in termurl_to_model:
-        annotation_model = termurl_to_model[term_url]
-        annotation_instance = annotation_model(TermURL=term_url)
+    annotations = Annotations(IsAbout=annotation_instance, Levels=levels)
+    return TSVAnnotations(
+        Description=description,
+        Levels={k: v["Label"] for k, v in levels.items() if "Label" in v},
+        Annotations=annotations,
+    )
 
-        description = ""
-        identifies = None
-        levels = None
-        transformation = None
 
-        if isinstance(annotation_instance, IsAboutParticipant):
-            description = "A participant ID"
-            identifies = "participant"
-        elif isinstance(annotation_instance, IsAboutSex):
-            description = "Sex information"
-            levels_data = parsed_output.get("Levels")
-            if levels_data:
-                levels = {
-                    level.strip().lower(): {
-                        "TermURL": levels_mapping[level.strip().lower()][
-                            "TermURL"
-                        ],
-                        "Label": levels_mapping[level.strip().lower()][
-                            "Label"
-                        ],
-                    }
-                    for level in levels_data
-                    if level.strip().lower() in levels_mapping
-                }
-        elif isinstance(annotation_instance, IsAboutAge):
-            description = "Age information"
-            transformation_data = parsed_output.get("Format")
-            if transformation_data:
-                if isinstance(transformation_data, list):
-                    transformation_key = transformation_data[0].strip().lower()
-                else:
-                    transformation_key = transformation_data.strip().lower()
+def handle_session(parsed_output: Dict[str, Any]) -> TSVAnnotations:
+    annotation_instance = IsAboutSession(TermURL=parsed_output["TermURL"])
+    description = "A session ID"
+    annotations = Annotations(
+        IsAbout=annotation_instance, Identifies="session"
+    )
+    return TSVAnnotations(Description=description, Annotations=annotations)
 
-                if transformation_key in transformation_mapping:
-                    transformation = transformation_mapping[transformation_key]
 
-        elif isinstance(annotation_instance, IsAboutSession):
-            description = "A session ID"
-            identifies = "session"
+def load_levels_mapping(mapping_file: str) -> Dict[str, Dict[str, str]]:
+    with open(mapping_file, "r") as file:
+        mappings = json.load(file)
+    return {
+        entry["label"]
+        .strip()
+        .lower(): {"TermURL": entry["identifier"], "Label": entry["label"]}
+        for entry in mappings
+    }
 
-        annotations_data = {
-            "IsAbout": annotation_instance,
-            "Identifies": identifies,
-            "Levels": levels,
-            "Transformation": transformation,
-        }
-        annotations = Annotations(**annotations_data)
 
-        tsv_annotations = TSVAnnotations(
-            Description=description,
-            Annotations=annotations,
-        )
+def process_parsed_output(
+    parsed_output: Dict[str, Union[str, Dict[str, str], None]],
+    levels_mapping: Mapping[str, Dict[str, str]],
+) -> Union[str, TSVAnnotations]:
+    termurl_to_function: Dict[
+        str, Callable[[Dict[str, Any]], TSVAnnotations]
+    ] = {
+        "nb:ParticipantID": handle_participant,
+        "nb:Age": handle_age,
+        "nb:Session": handle_session,
+    }
 
-        return tsv_annotations
+    termurl_to_function_with_levels: Dict[
+        str,
+        Callable[
+            [Dict[str, Any], Mapping[str, Dict[str, str]]], TSVAnnotations
+        ],
+    ] = {
+        "nb:Sex": handle_categorical,
+        "nb:Diagnosis": handle_categorical,
+    }
+
+    if isinstance(parsed_output, dict):
+        term_url = parsed_output.get("TermURL")
+        if isinstance(term_url, str):
+            if term_url in termurl_to_function:
+                handler_function = termurl_to_function[term_url]
+                return handler_function(parsed_output)
+            elif term_url in termurl_to_function_with_levels:
+                handler_function_with_levels = termurl_to_function_with_levels[
+                    term_url
+                ]
+                return handler_function_with_levels(
+                    parsed_output, levels_mapping
+                )
+            else:
+                return (
+                    f"Error: No handler function found for TermURL: {term_url}"
+                )
+        else:
+            return "Error: TermURL is missing from the parsed output"
     else:
-        return (
-            f"Error: No annotation model found for TermURL: {term_url}"
-            if term_url
-            else "Error: TermURL is missing from the parsed output"
-        )
+        return "Error: parsed_output is not a dictionary"
 
 
 def update_json_file(
     data: Union[str, TSVAnnotations], filename: str, target_key: str
 ) -> None:
     if isinstance(data, TSVAnnotations):
-        data_dict = data.model_dump(exclude_none=True)
+        data_dict = data.dict(exclude_none=True)
     else:
         data_dict = {"error": data}
 
     try:
         with open(filename, "r") as file:
-            file_data = json.load(file)
+            file_data: Dict[str, Any] = json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
         file_data = {}
     file_data[target_key] = data_dict
@@ -217,19 +222,32 @@ if __name__ == "__main__":
     json_file = "output.json"
     tsv_to_json(file_path, json_file)
 
+    # Load the levels mapping from a JSON file
+    levels_mapping_file = "parsing/bin/diagnosisTerms.json"
+    levels_mapping = load_levels_mapping(levels_mapping_file)
+
     # LLM-Categorization magic
 
     # Create output for each column
     for key, value in columns_dict.items():
         print(key, value)
-        # parsed_output = chain.invoke({"column": key, "content": value})
-        # print(parsed_output)
-        # parsed_output = {"TermURL": "nb:Sex", "Levels": ["male", "female"]}
-        parsed_output = {"TermURL": "nb:ParticipantID"}
-        # parsed_output = {"TermURL": "nb:Age", \
-        #                   "Format":"europeanDecimalValue"}
-        # parsed_output ={"TermURL": "nb:Session" }
-        result = process_parsed_output(parsed_output)
+        parsed_output: Dict[str, Union[str, Dict[str, str], None]] = {
+            "TermURL": "nb:Age",
+            "Format": "europeanDecimalValue",
+        }
+        # parsed_output: Dict[str, Union[str, Dict[str, str], None]] = {
+        #    "TermURL": "nb:Sex",
+        #    "Levels": {"M": "male", "F": "female"},
+        # }
+        # parsed_output = {"TermURL": "nb:ParticipantID"}
+        # parsed_output = {"TermURL": "nb:Session"}
+        # parsed_output = {
+        #    "TermURL": "nb:Diagnosis",
+        #    "Levels": {
+        #        "MDD": "Major depressive disorder",
+        #        "CTRL": "healthy control",
+        #    },
+        # }
+        result = process_parsed_output(parsed_output, levels_mapping)
         print(result)
-        print(type(result))
         update_json_file(result, "output.json", key)
