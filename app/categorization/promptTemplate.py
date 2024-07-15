f3376f7b69c875639abc5a3df038a30301f46140
Tvisha Vedant <122543856+tvilight4@users.noreply.github.com>
2024-07-15 15:25:18 +0530

    Update app/categorization/promptTemplate.py
    Co-authored-by: barbarastrasser <162976974+barbarastrasser@users.noreply.github.com>

--- app/categorization/promptTemplate.py
+++ app/categorization/promptTemplate.py
@@ -79,20 +79,3 @@ DiagnosisPrompt = PromptTemplate(
 )
 
 
-
-# Aprompt = PromptTemplate(
-#      template="""Given the column data {column}: {content},
-#     Instructions: Based on the provided sample data, please evaluate if this column is an assessment tool. 
-#     give yes or no for {question}
-#     Consider the following characteristics of assessment tools in your evaluation:
-#     Purpose: Does the column aim to measure or evaluate something specific?
-#     Format: Is the data structured in a way that suggests a test, survey, or questionnaire (e.g., Likert scale, multiple-choice, ratings)?
-#     Consistency: Is there a consistent format or scale used throughout the data (e.g., scores out of 10, ratings from 1 to 5)?
-#     Content: Does the content of the data suggest evaluation metrics (e.g., scores, ratings, evaluation criteria)?
-#     Metadata: Are there labels, descriptions, or metadata that indicate the column's purpose as an assessment tool?
-#     The sample data might include session or participant IDs - these are not assessment tools.
-#     Do not give any explanation in the output. Simply provide yes or no for .
-#     """,
-        
-#     input_variables=["column", "content", "question"],
-# )
