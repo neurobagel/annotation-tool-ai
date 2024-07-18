46bedf2c0545cfeb83d6ace4160ac4ed7e3884d2
Barbara Strasser-Kirchweger <barbara.strasser-kirchweger@plus.ac.at>
2024-07-18 10:54:19 +0200

    add utils

--- app/utils/file_operations/file_operations_interface.py
+++ app/utils/file_operations/file_operations_interface.py
@@ -6,3 +6,7 @@ class FileOperations(ABC):
     @abstractmethod
     def execute(self, *args: Any, **kwargs: Any) -> None:
         pass
+
+    @abstractmethod
+    def get_column_strings(self) -> Any:
+        pass
