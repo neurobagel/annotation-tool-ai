6a572cba55c8a10f5768aa1e8e4c14028d548897
Barbara Strasser-Kirchweger <barbara.strasser-kirchweger@plus.ac.at>
2024-07-18 11:16:44 +0200

    add file file_operations

--- app/utils/file_operations/file_operations_factory.py
+++ app/utils/file_operations/file_operations_factory.py
@@ -1,8 +1,8 @@
 from typing import Type, Dict
 from app.utils.file_operations.convert_tsv_to_dict import ConvertTSVToDict
-from app.utils.file_operations.file_operations_interface import FileOperations
 from app.utils.file_operations.tsv_to_json import TSVToJSON
 from app.utils.file_operations.update_json_file import UpdateJSONFile
+from app.utils.file_operations.file_operations_interface import FileOperations
 
 
 class FileOperationsFactory:
