7418449e9a5b31b9ab180ee998582abc1efc74cb
Atharv Vedant <tvishavedant@gmail.com>
2024-08-12 18:11:38 +0530

    final updates

--- app/parsing/json_parsing.py
+++ app/parsing/json_parsing.py
@@ -227,15 +227,28 @@ def handle_assessmentTool(
 
 
 
-def load_levels_mapping(mapping_file: str) -> Dict[str, Dict[str, List[str]]]:
+def load_levels_mapping(mapping_file: str) -> Dict[str, Dict[str, str]]:
     with open(mapping_file, "r") as file:
         mappings = json.load(file)
-    return {
-        entry["label"]
-        .strip()
-        .lower(): {"TermURL": entry["identifier"], "Label": entry["label"]}
-        for entry in mappings
-    }
+        
+    levels_mapping = {}
+    for entry in mappings:
+        label_key = entry.get("label", "").strip().lower()
+        identifier_key = entry.get("identifier")
+        
+        if not label_key:
+            print(f"Warning: Missing or empty 'label' in entry: {entry}")
+            continue
+        
+        if not identifier_key:
+            # print(f"Warning: Missing 'identifier' for label '{label_key}' in entry: {entry}")
+            # Optionally, you can skip this entry or assign a default value
+            identifier_key = "default_identifier"
+
+        levels_mapping[label_key] = {"TermURL": identifier_key, "Label": entry["label"]}
+    
+    return levels_mapping
+
 
 def load_assessmenttool_mapping(
     mapping_file: str,
@@ -269,7 +282,7 @@ def process_parsed_output(
         )
     elif code_system == "snomed":
         print("Using SNOMED CT terms for assessment tool annotation.")
-        assessmenttool_mapping_file = "app/parsing/measurementTerms.json"
+        assessmenttool_mapping_file = "app/parsing/abbreviations_measurementTerms.json"
         assessmenttool_mapping = load_levels_mapping(
             assessmenttool_mapping_file
         )
