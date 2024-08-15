e9111f4746edb86f7ebab9ce87abbd3a61e8a41a
Atharv Vedant <tvishavedant@gmail.com>
2024-08-15 08:29:16 +0530

    cyhanging file path

--- app/categorization/llm_helper.py
+++ app/categorization/llm_helper.py
@@ -161,7 +161,7 @@ def Diagnosis_Level(unique_entries:dict,code_system: str,levels):
             return ["left for user"]
         
 # Path to your JSON file
-    file_path = '/home/atharv-vedant/annotation-tool-ai/rag_documents/abbreviation_to_labels.json'
+    file_path = '/annotation-tool-ai/rag_documents/abbreviation_to_labels.json'
 
 # Load the JSON data
     data = load_dictionary(file_path)
